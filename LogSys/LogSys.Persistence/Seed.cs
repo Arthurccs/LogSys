17950c2f3edf03027137d14351ee971edfd3e4fc
Carlos Ruiz <carlosruiz21@gmail.com>
2021-03-31 04:52:23 -0500

    adding seed to test walking skeleton

--- LogSys/LogSys.Persistence/Seed.cs
+++ LogSys/LogSys.Persistence/Seed.cs
@@ -1,11 +1,66 @@
-﻿using System;
+﻿using LogSys.Domain;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace LogSys.Persistence
 {
 	public class Seed  
 	{
+		public static async Task SeedData(DataContext context)
+		{
+			if (context.Logs.Any())
+			{
+				return;
+			}
+			var logs = new List<Log>
+			{
+				new Log
+				{
+					Title ="Test Title1",
+					Message = "Message 1",
+					Datetimecreation = DateTime.Now,
+					Level = "error",
+					Userid = "CRUIZ"
+				},
+				new Log
+				{
+					Title ="Test Title2",
+					Message = "Message 2",
+					Datetimecreation = DateTime.Now,
+					Level = "error",
+					Userid = "ARTURO"
+				},
+				new Log
+				{
+					Title ="Test Title3",
+					Message = "Message 3",
+					Datetimecreation = DateTime.Now.AddMonths(12),
+					Level = "error",
+					Userid = "CRUIZ"
+				},
+				new Log
+				{
+					Title ="Test Title4",
+					Message = "Message 4",
+					Datetimecreation = DateTime.Now.AddMonths(1),
+					Level = "error",
+					Userid = "CRUIZ"
+				},
+				new Log
+				{
+					Title ="Test Title5",
+					Message = "Message 5",
+					Datetimecreation = DateTime.Now.AddMonths(2),
+					Level = "error",
+					Userid = "CRUIZ"
+				},
+			};
+			await context.Logs.AddRangeAsync(logs);
+			await context.SaveChangesAsync();
+
+		}
 
 	}
 }
--- LogSys/LogSys.WepApi/Program.cs
+++ LogSys/LogSys.WepApi/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LogSys.Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +13,25 @@ namespace LogSys.WepApi
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			var host = CreateHostBuilder(args).Build();
+
+			using var scope = host.Services.CreateScope();
+			var services = scope.ServiceProvider;
+			try
+			{
+				var context = services.GetRequiredService<DataContext>();
+				await Seed.SeedData(context);
+
+			}
+			catch (Exception ex)
+			{
+				var logger = services.GetRequiredService<ILogger<Program>>();
+				logger.LogError(ex, "An error ocurring during Seed");
+				throw;
+			}
+
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
