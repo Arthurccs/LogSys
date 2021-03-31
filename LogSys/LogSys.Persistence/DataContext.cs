490989c54da275eba16a4fac8c54001efceea8a1
Carlos Ruiz <carlosruiz21@gmail.com>
2021-03-31 01:59:27 -0500

    adding context file

---
+++ LogSys/LogSys.Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using LogSys.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogSys.Persistence
+{
+	public class DataContext: DbContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{ 
+		}
+		public DbSet<Log> Logs { get; set; }
+
+	}
+}
